10c66a03243ae9af9747a70a671be119133ce142
Jacob Bogart <45608610+jacobogart@users.noreply.github.com>
2019-05-31 17:20:17 -0600

    Delete utilities
    added to git ignore

--- src/api/utilities.js
+++
@@ -1,5 +0,0 @@
-export const url = 'https://cors-anywhere.herokuapp.com/https://locator.hiv.gov/data?';
-
-export const googleKey = 'AIzaSyDHKnndIp7I8oymrxO0sDwpQUdZvfN1sqc';
-
-export const geocodeUrl = 'https://maps.googleapis.com/maps/api/geocode/json?address='
