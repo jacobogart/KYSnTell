3668029b87257671eacebab1b03ec4c5370bbc0c
jacobogart <jacobogart@gmail.com>
2019-06-03 09:05:26 -0600

    Add details features

--- src/containers/DetailsPage/DetailsPage.jsx
+++ src/containers/DetailsPage/DetailsPage.jsx
@@ -5,8 +5,15 @@ class DetailsPage extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      showContacts: false
+      showContacts: false,
+      diagnosis: '',
+      timeFrame: '',
+      additionalNotes: ''
     }
+  }
+
+  handleChange = (e) => {
+    this.setState({ [e.target.name]: e.target.value });
   }
 
   toggleContacts = () => {
@@ -16,13 +23,48 @@ class DetailsPage extends Component {
   render() {
     const contactList = this.props.contacts.map(contact => <li>{contact}</li>)
     const contactsDisplay = this.state.showContacts 
-      ? <ul className="contact-list">
+      ? <ol className="contact-list">
           {contactList}
-        </ul>
+        </ol>
       : <p onClick={this.toggleContacts}>{`${this.props.contacts[0]}...`}</p>
     return (
-      <div>
+      <div className="DetailsPage">
         {contactsDisplay}
+        <form classNall='details-form' onSubmit={this.handleSubmit}>
+          <select
+            name="timeFrame"
+            onChange={this.handleChange}
+            className="search-input"
+          >
+            <option value="10">10 miles</option>
+            <option value="20">20 miles</option>
+            <option value="30">30 miles</option>
+            <option value="40">40 miles</option>
+            <option value="50">50 miles</option>
+          </select>
+          <select
+            name="distance"
+            onChange={this.handleChange}
+            className="search-input"
+          >
+            <option value="10">10 miles</option>
+            <option value="20">20 miles</option>
+            <option value="30">30 miles</option>
+            <option value="40">40 miles</option>
+            <option value="50">50 miles</option>
+          </select>
+          <input
+            type="text"
+            name="aditionalNotes"
+            placeholder="Additional notes..."
+            className="search-input"
+            onChange={this.handleChange}
+          />
+          <button type="submit" >
+            Use zipcode
+        </button>
+        </form>
+
       </div>
     );
   }
