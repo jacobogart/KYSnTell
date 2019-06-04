c6f7ffc8eb15fce5ccffc4284f394bfeb28590e3
jacobogart <jacobogart@gmail.com>
2019-06-03 20:04:22 -0600

    Add fetchLatLong tests

--- src/mockData.js
+++ src/mockData.js
@@ -22,6 +22,18 @@ export const distanceResult = {
   ]
 }
 
-export const distance = '18.3 mi'
+export const distance = '18.3 mi';
+
+export const latLongResult = {
+  results: [
+    {
+      geometry: {
+        location: origin
+      }
+    }
+  ]
+}
 
 export const fetchDistanceURL = "https://cors-anywhere.herokuapp.com/https://maps.googleapis.com/maps/api/distancematrix/json?origins=38.9119463,-94.4136213&destinations=39.1952922,-94.6817269&units=imperial&key=AIzaSyDHKnndIp7I8oymrxO0sDwpQUdZvfN1sqc"
+
+export const fetchLatLongURL = "https://maps.googleapis.com/maps/api/geocode/json?address=64081&key=AIzaSyDHKnndIp7I8oymrxO0sDwpQUdZvfN1sqc"
