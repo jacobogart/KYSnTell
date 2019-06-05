6d61dc2f0ccb74aa84f2c8c2e5792cfe1b139560
jacobogart <jacobogart@gmail.com>
2019-06-04 19:41:09 -0600

    Add Splash snapshot test

---
+++ src/components/Splash/Splash.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { Splash } from './Splash';
+
+describe('Splash', () => {
+  it('should match snapshot', () => {
+    const wrapper = shallow(<Splash />)
+    expect(wrapper).toMatchSnapshot();
+  });
+})
